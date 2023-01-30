7e59020e3a3c6de39892a9867e0faab03b39161d
Mono-Blaine <samer.abki@gmail.com>
2023-01-30 08:49:12 +0200

    fixed bg-color not applying to full side nav

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import SideNavigation from "./navigation/SideNavigation";
 function Layout(props: any): any {
   return (
     <Fragment>
-      <div className="flex h-screen flex-col bg-secondary md:flex-row">
+      <div className="flex h-full flex-col bg-secondary md:flex-row">
         <SideNavigation />
         <main className="w-full">{props.children}</main>
       </div>
