1537968fc62a7fbc0e5c49b97b66d8efec796de5
TamerAlbouz <tea06@mail.aub.edu>
2023-04-09 19:37:02 +0300

    Added ClinicInfo hook

--- components/pages/overview/cards/ClinicInfo.tsx
+++ components/pages/overview/cards/ClinicInfo.tsx
@@ -1,6 +1,7 @@
 import { BsFillTelephoneFill, BsPaperclip } from "react-icons/bs";
 import { BiCurrentLocation } from "react-icons/bi";
 import CardWrapper from "../../../card/CardWrapper";
+import { useClinicQuery } from "../../../../hooks/queries/useClinicQuery";
 
 const arr = [
   { title: "Horizon Therapy Center", icon: BsPaperclip },
@@ -9,12 +10,20 @@ const arr = [
 ];
 
 function ClinicInfo() {
+  const { data } = useClinicQuery();
+
+  const arr = [
+    { title: data?.name, icon: BsPaperclip },
+    { title: data?.location, icon: BiCurrentLocation },
+    { title: data?.phone, icon: BsFillTelephoneFill },
+  ];
+
   return (
     <CardWrapper title="CLINIC INFO">
       <div className="flex flex-col gap-5 font-medium text-textColor">
         {arr.map((item) => (
           <div className="flex items-center gap-3" key={item.title}>
-            <item.icon className="text-xl " />
+            <item.icon className="text-xl" />
             <p>{item.title}</p>
           </div>
         ))}
